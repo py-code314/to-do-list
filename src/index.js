2df45dc9b077eb336aad261903303a41cf432b5e
srikk <122169588+py-code314@users.noreply.github.com>
2025-03-27 13:35:49 -0400

    feat: Add <dialog> for new task form

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 import './styles/styles.css'
 // import JS modules
 import './scripts/newTodo'
+import { showNewTaskForm } from './scripts/addTaskForm';
+
+// Event listener for Add Task button
+const addTaskButton = document.querySelector('#addTask');
+// console.log(addTaskButton);
+
+addTaskButton.addEventListener('click', showNewTaskForm);
+
+
--- src/scripts/addTaskForm.js
+++ src/scripts/addTaskForm.js
@@ -1,1 +1,65 @@
 // show form when user clicks Add Task button
+
+import closeIcon from '../assets/images/icon-close.svg'
+
+const content = document.querySelector('#content');
+
+export function showNewTaskForm() {
+  console.log('show modal');
+  // Dialog modal
+  const modal = document.createElement('dialog');
+  modal.classList.add('modal');
+
+  // Create form
+  // const form = document.createElement('form');
+  // form.classList.add('form');
+  // form.method = 'dialog';
+  const form = Object.assign(document.createElement('form'), {
+    className: 'form',
+    method: 'dialog'
+  });
+
+  // Create form header
+  const header = document.createElement('header');
+  header.classList.add('form__header');
+
+  // Create form title
+  const h2 = document.createElement('h2');
+
+  // Create close button
+  const closeButton = Object.assign(document.createElement('button'), {
+    id: 'closeButton',
+    className: 'button button--close',
+    type: 'button',
+    value: 'close',
+  });
+
+  // Create image element
+  const image = Object.assign(document.createElement('img'), {   
+    className: 'form__image',
+    src: `${closeIcon}`,
+    alt: 'Close',
+    title: 'Close'
+  });
+  closeButton.appendChild(image)
+
+  // Create paragraph element
+  const para = document.createElement('p');
+  para.textContent = 'Required fields.';
+  const strong = document.createElement('strong');
+  strong.textContent = '*'
+  para.appendChild(strong)
+  
+  // Add h2, button, para to form header
+  header.append(h2, closeButton, para)
+
+  //TODO: change variable names
+
+  form.appendChild(header)
+  modal.appendChild(form)
+  content.appendChild(modal)
+
+  console.log('modal created');
+  modal.showModal()
+
+}
